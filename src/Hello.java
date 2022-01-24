849aeee7af5b89ecd95c4889b9dabd5880429df2
neyuc <nm.yucesoy@gmail.com>
2022-01-24 10:39:09 -0500

    b25 learning

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("B 25 is learning git nad gitub");
     }
 
 }
